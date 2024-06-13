6f7c95322335ba6485231168ac825a1547cd9244
you@example.com <you@example.com>
2024-06-13 11:00:49 +0530

    done project

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './config/firebase';
-import Sidebar, { Stat } from './component/Dashboard';
+import Dashboard, { Stat } from './component/Dashboard';
 import CyberSecurity from './pages/CyberSecurity';
 import AppDev from './pages/AppDev';
 import WebDev from './pages/WebDev';
@@ -24,7 +24,7 @@ const App = () => {
     <div className='bg-blue-900'>
       <BrowserRouter>
         <Routes>
-          <Route path='/service' element={<Service />}>
+          <Route path='/service' element={user ? <Dashboard /> : <Navigate to="/login" />}>
             <Route index element={<Stat />} />
             <Route path='webdev' element={<WebDev />} />
             <Route path='appdev' element={<AppDev />} />
@@ -37,9 +37,9 @@ const App = () => {
             <Route path='about' element={<AboutUs />} />
             <Route path='contact' element={<ContactUs />} />
           </Route>
-          <Route path="/register" element={user ? <Navigate to="/login" /> : <Register />} />
+          <Route path="/register" element={user ? <Navigate to="/service" /> : <Register />} />
           <Route path="/login" element={user ? <Navigate to="/service" /> : <Loginpage />} />
-          <Route path="/" element={<Navigate to={user ? "/service" : "/login"} />} />
+          <Route path="/webdev" element={user ? <WebDev /> : <Navigate to="/login" />} />
         </Routes>
       </BrowserRouter>
     </div>
